7ce5f6a1e554a7d452bef31d2851965faedfb102
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-29 19:06:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-programmer_hash[:yukihiro_matsumoto] = :yukihiro_matsumoto => :known_for => "Ruby" :languages = ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto] = :yukihiro_matsumoto
+
 #=> [:known_for => "Ruby", :languages => ["LISP", "C"]]
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
